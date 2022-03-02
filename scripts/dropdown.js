0822c15bed08b7fde0acbab934a06c5d5d6741e3
Ritesh Saha <riteshsaha214@gmail.com>
2022-03-02 10:09:45 +0530

    [ initial commit ]

---
+++ scripts/dropdown.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('menu-icon')[0].addEventListener('click', function() {
+    this.classList.toggle('active-menu');
+    document.getElementsByClassName('container')[0].classList.toggle('active-menu');
+    document.getElementsByClassName('nav-1')[0].classList.toggle('active-menu');
+    document.getElementsByClassName('total-center')[0].classList.toggle('active-menu');
+    document.getElementsByClassName('menu-options')[0].classList.toggle('active-menu');
+    document.getElementsByClassName('mobile-menu')[0].classList.toggle('active');
+    document.getElementsByClassName('logo')[0].classList.toggle('active');
+});
