0d2bd5a1d5b588c0b7d1e63256ed37bc5e188800
Ritesh Saha <riteshsaha214@gmail.com>
2022-03-03 20:39:21 +0530

    [ fixed margin ]

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -2,5 +2,4 @@
 document.getElementsByClassName('menu-btn')[0].addEventListener('click', function() {
     this.classList.toggle('active');
     document.getElementsByClassName('menu')[0].classList.toggle('active');
-    document.getElementsByClassName('main-content')[0].classList.toggle('active');
 });
