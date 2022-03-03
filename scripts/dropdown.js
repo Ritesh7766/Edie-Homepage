9f21ebb1841f970f9a2d654485724bba27e8235b
Ritesh Saha <riteshsaha214@gmail.com>
2022-03-03 20:03:18 +0530

    [ goofy animation added ]

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,10 +1,4 @@
-document.getElementsByClassName('menu-icon')[0].addEventListener('click', function() {
-    this.classList.toggle('active-menu');
-    document.getElementsByClassName('container')[0].classList.toggle('active-menu');
-    document.getElementsByClassName('nav-1')[0].classList.toggle('active-menu');
-    document.getElementsByClassName('total-center')[0].classList.toggle('active-menu');
-    document.getElementsByClassName('menu-options')[0].classList.toggle('active-menu');
-    document.getElementsByClassName('mobile-menu')[0].classList.toggle('active');
-    document.getElementsByClassName('logo')[0].classList.toggle('active');
-    document.getElementsByClassName('main-content')[0].classList.toggle('active');
+document.getElementsByClassName('menu-btn')[0].addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementsByClassName('menu')[0].classList.toggle('active');
 });
