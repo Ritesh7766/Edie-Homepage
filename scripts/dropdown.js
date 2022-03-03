45b6b39cc3175dcfee989c1ac6318b6e74d8d389
Ritesh Saha <riteshsaha214@gmail.com>
2022-03-03 20:31:48 +0530

    [ initial commit ]

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,4 +1,6 @@
+
 document.getElementsByClassName('menu-btn')[0].addEventListener('click', function() {
     this.classList.toggle('active');
     document.getElementsByClassName('menu')[0].classList.toggle('active');
+    document.getElementsByClassName('main-content')[0].classList.toggle('active');
 });
